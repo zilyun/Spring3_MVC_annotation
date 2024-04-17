5db92b02543ec0a652ef4502b2634cf8bddded29
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-04-17 14:27:15 +0900

    Study: Spring MVC annotation 교육 - 1

---
+++ src/main/java/com/naver/myhome/Spring3Mvc3Application.java
@@ -0,0 +1,13 @@
+package com.naver.myhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3Mvc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3Mvc3Application.class, args);
+	}
+
+}
